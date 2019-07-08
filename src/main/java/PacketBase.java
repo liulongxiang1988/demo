dc147d4ca795547f49bf8922fc6353feff6886db
liulongxiang1988 <40624936+liulongxiang1988@users.noreply.github.com>
2019-07-08 10:44:36 +0800

    Update PacketBase.java
    rrrrr

--- src/main/java/PacketBase.java
+++ src/main/java/PacketBase.java
@@ -5,6 +5,9 @@ public abstract class PacketBase implements PacketAction{
     private  final static Map<String,Class> map = new HashMap();
 
     public void register(String key, Class < ? extends PacketBase> clazz){
+        
+        
+        
         map.put(key,clazz);
     }
 }
